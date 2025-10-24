85b014c99fbde2fb2d901955854b48978bf9042f
Arii-SF <msandovalf2@miumg.edu.gt>
2025-10-23 22:48:17 -0600

    feat: Regreso versión anterior

--- Mappings/ContactoProfile.cs
+++ Mappings/ContactoProfile.cs
@@ -28,11 +28,8 @@ namespace CrmContactsApi.Mappings
 
             CreateMap<CalificacionContacto, CalificacionDto>();
             CreateMap<CreateCalificacionRequest, CalificacionContacto>();
-
             CreateMap<Contacto, ContactoConCalificacionDto>()
-                .IncludeBase<Contacto, ContactoDto>()
-                .ForMember(dest => dest.NombreUsuarioCreacion, opt => opt.Ignore())
-                .ForMember(dest => dest.NombreUsuarioActualizacion, opt => opt.Ignore());
+                .IncludeBase<Contacto, ContactoDto>();
         }
     }
 }
--- obj/Debug/net9.0/CrmContactsApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/CrmContactsApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrmContactsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ebdc14d85cda7261784b075d92770ac008c5540")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+240cad0c1d1d0b7075a22663ad6cf26dc3aea7f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrmContactsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrmContactsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
